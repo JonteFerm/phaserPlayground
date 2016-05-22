7e34fd63250ad56476977537d5194a0a82158595
JonteFerm <jonte_ferm@hotmail.com>
2016-05-22 20:32:23 +0200

    Prototyping

--- js/game.js
+++ js/game.js
@@ -2,6 +2,23 @@ var TopDownGame = TopDownGame || {};
 
 TopDownGame.Game = function(){};
 
+
+Player = function(game, x, y){
+	Phaser.Sprite.call(this, game, x, y, 'player');
+	this.inventory = ['neger'];
+};
+
+Player.prototype = Object.create(Phaser.Sprite.prototype);
+
+Item = function(game, x, y, sprite){
+	Phaser.Sprite.call(this, game, x, y, sprite);
+	this.gameProperties = {
+		itemLevel: 0,
+	};
+};
+
+Item.prototype = Object.create(Phaser.Sprite.prototype);
+
 TopDownGame.Game.prototype = {
 	create: function(){
 		this.map = this.game.add.tilemap('dungeontest');
@@ -20,9 +37,11 @@ TopDownGame.Game.prototype = {
 	    this.createItems();
 	    this.createDoors();
 
-	    var result = this.findObjectsByType('playerStart', this.map, 'objectLayer');
-
-	    this.player = this.game.add.sprite(result[0].x, result[0].y, 'player');
+	    var playerStart = this.findObjectsByType('playerStart', this.map, 'objectLayer')[0];
+	    this.player = new Player(this.game, playerStart.x, playerStart.y);
+	    this.game.add.existing(this.player);
+	    this.player.animations.add('right', [0,1], 10, true);
+	    this.player.animations.add('left', [2,3], 10, true);
 	    this.game.physics.arcade.enable(this.player);
 	    this.game.camera.follow(this.player);
 
@@ -31,14 +50,12 @@ TopDownGame.Game.prototype = {
 			down: this.game.input.keyboard.addKey(Phaser.Keyboard.S),
 			left: this.game.input.keyboard.addKey(Phaser.Keyboard.A),
 			right: this.game.input.keyboard.addKey(Phaser.Keyboard.D)
-		}
-
-
-	    
+		}	    
 	},
 
 	update: function(){
 		this.game.physics.arcade.collide(this.player, this.blockLayer);
+		this.game.physics.arcade.overlap(this.player, this.items, this.pickupItem, null, this);
 		this.player.body.velocity.y = 0;
 		this.player.body.velocity.x = 0;
 
@@ -52,18 +69,35 @@ TopDownGame.Game.prototype = {
 			this.player.body.velocity.y = 100;
 		}else if(this.wasd.left.isDown){
 			this.player.body.velocity.x = -100;
+			this.player.animations.play("left");
 		}else if(this.wasd.right.isDown){
 			this.player.body.velocity.x = 100;
+			this.player.animations.play("right");
+		}else{
+			this.player.animations.stop();
 		}
+	},
+
+	pickupItem: function(player,item){
+		player.inventory.push(item.key);
+		console.log(item);
+		console.log("Player inventory: " + this.player.inventory);
+		item.destroy();
 	},
 
 	createItems: function(){
 		this.items = this.game.add.group();
 		this.items.enableBody = true;
-		var item;
 		result = this.findObjectsByType('item', this.map, 'objectLayer');
 		result.forEach(function(element){
-			this.createFromTiledObject(element, this.items);
+			var newItem = new Item(this.game, element.x, element.y, element.properties.sprite);
+
+			Object.keys(element.properties).forEach(function(key){
+				newItem[key] = element.properties[key];
+			});
+			
+			this.items.add(newItem);
+
 		}, this);
 	},
 
@@ -73,7 +107,12 @@ TopDownGame.Game.prototype = {
 		result = this.findObjectsByType('door', this.map, 'objectLayer');
 
 		result.forEach(function(element){
-			this.createFromTiledObject(element, this.doors);
+			var newDoor = this.doors.create(element.x, element.y, element.properties.sprite);
+
+			Object.keys(element.properties).forEach(function(key){
+				newDoor[key] = element.properties[key];
+			});
+
 		}, this);
 	},
 
@@ -88,14 +127,4 @@ TopDownGame.Game.prototype = {
 		});	
 		return result;
 	},
-
-	createFromTiledObject: function(element, group){
-		var sprite = group.create(element.x, element.y, element.properties.sprite);
-
-		Object.keys(element.properties).forEach(function(key){
-			sprite[key] = element.properties[key];
-		});
-	}
-
-
 }
--- js/preload.js
+++ js/preload.js
@@ -6,7 +6,8 @@ TopDownGame.Preload.prototype = {
 	preload: function(){
 		this.load.tilemap('dungeontest', 'assets/dungeontest/tilemaps/tiletest.json', null, Phaser.Tilemap.TILED_JSON);
 		this.load.image('gameTiles', 'assets/dungeontest/images/tiles2.png');
-		this.load.image('player', 'assets/dungeontest/images/player.png');
+		//this.load.image('player', 'assets/dungeontest/images/player.png');
+		this.load.spritesheet('player', 'assets/dungeontest/images/playersprite.png', 32, 32);
 		this.load.image('gate', 'assets/dungeontest/images/gate.png');
 		this.load.image('goldenkey', 'assets/dungeontest/images/goldenkey.png');
 
